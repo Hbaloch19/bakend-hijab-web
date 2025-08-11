dd5c84a1285429c4bcf6423822bfb425c9f33bb6
Hanadi Baloch <167871824+Hbaloch19@users.noreply.github.com>
2025-08-11 11:17:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const path = require('path');
 const app = express();
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/auth-app', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB error:", err));
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB error:", err));
 
 // User Schema
 const UserSchema = new mongoose.Schema({
